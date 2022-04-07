079fb9b51bf6defe1e3939d65043245426bafdc9
bindisanjay <bindisanjay@gmail.com>
2022-04-06 20:23:14 -0500

    Added file

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var countryRouter = require('./routes/country');
-
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -15,6 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use('/country',countryRouter);
+app.use('/addmods',addmodsRouter);
+app.use('/selector',selectorRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
